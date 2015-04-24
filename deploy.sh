c38d62296429ab3fc31df3b9c0b05399ca846fb4
Andreas Gyr <andygyr@gmx.ch>
2015-04-24 07:49:23 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,4 @@ git config user.email "andygyr@gmx.ch"
 git config --global url."https://".insteadOf git://
 
 git checkout -b prod
-git branch
 git push --force --quiet "https://${GH_TOKEN}@github.com/Cogax/SysVentoryServer.git" prod:prod > /dev/null 2>&1
