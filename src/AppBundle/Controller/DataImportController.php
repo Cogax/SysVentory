747e5f4f219719c46a3bd2a9bbe171b82c7ca48a
Andreas Gyr <andygyr@gmx.ch>
2015-04-29 23:35:29 +0200

    dataimport controller created

---
+++ src/AppBundle/Controller/DataImportController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+
+class DataImportController extends Controller
+{
+    public function importAction()
+    {
+
+    }
+
+}
