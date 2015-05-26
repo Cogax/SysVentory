1f2864ab957ec0c0f3e4b4fd07f284db837b763c
Andreas Gyr <andygyr@gmx.ch>
2015-05-26 18:10:55 +0200

    give back xml in case the import fails

--- src/AppBundle/Controller/ImportController.php
+++ src/AppBundle/Controller/ImportController.php
@@ -18,19 +18,22 @@ class ImportController extends Controller
 
         // Check request http method
         if($request->getMethod() != 'POST') {
-            return new Response('Invalid method!', Response::HTTP_METHOD_NOT_ALLOWED);
+            //return new Response('Invalid method!', Response::HTTP_METHOD_NOT_ALLOWED);
+            return new Response($request->getContent(), Response::HTTP_METHOD_NOT_ALLOWED);
         }
 
         // Check request http header "Content-Type"
         if($request->getContentType() != 'xml') {
-            return new Response('Invalid content-type: '.$request->getContentType(), Response::HTTP_UNSUPPORTED_MEDIA_TYPE);
+            //return new Response('Invalid content-type: '.$request->getContentType(), Response::HTTP_UNSUPPORTED_MEDIA_TYPE);
+            return new Response($request->getContent(), Response::HTTP_UNSUPPORTED_MEDIA_TYPE);
         }
 
         // Check if XML is valid
         $xml = $request->getContent();
         $xsdFilename = $this->get('kernel')->locateResource("@AppBundle/Resources/schema/composition.xsd");
         if(!$this->get("app.xsd_validator")->idValid($xml, $xsdFilename)) {
-            return new Response('XML is not valid!', Response::HTTP_BAD_REQUEST);
+            //return new Response('XML is not valid!', Response::HTTP_BAD_REQUEST);
+            return new Response($request->getContent(), Response::HTTP_BAD_REQUEST);
         }
 
         // Store Composition
