934e91d2b8f3d7d560acaf63478eb3dc6350648a
Andreas Gyr <andygyr@gmx.ch>
2015-06-02 17:54:47 +0200

    collector command edited

--- src/AppBundle/Service/Util/Collector.php
+++ src/AppBundle/Service/Util/Collector.php
@@ -5,12 +5,12 @@ class Collector {
     const HOST = "172.17.82.42";
     const USERNAME = "administrator";
     const PASSWORD = "Password1";
-
+    const DOMAIN = "zbw.local";
     /**
      * @param string $netRange
      */
     public function scan($netRange) {
-        $cmd = '/srv/script/discovery.sh '.$netRange.' '.self::USERNAME.' '.self::PASSWORD.' '.self::HOST;
+        $cmd = "/srv/script/discovery.sh ".$netRange." ".self::USERNAME." ".self::PASSWORD." ".self::HOST." '".self::DOMAIN."'";
         $return = exec($cmd);
         die($return);
     }
