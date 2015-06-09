8de1f246b753f8940d9ebc9974e857902f5dcbac
Andreas Gyr <andygyr@gmx.ch>
2015-06-09 12:10:16 +0200

    added statistic on frontpage

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,7 +9,15 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('AppBundle:Default:index.html.twig', array(
-                // ...
-            ));    }
+            'compositionHistoryStatistics' => $this->getCompositionHistoryStatisticsData(),
+        ));
+    }
+
+    private function getCompositionHistoryStatisticsData() {
+        $conn = $this->container->get('database_connection');
+        $query =  "SELECT count(*) as quantity, DATE(time) as dateday from compositionhistory GROUP BY dateday;";
+        $stmt = $conn->query($query);
+        return $stmt->fetchAll();
+    }
 
 }
