40feaeb8822bc3b44988642743af992f6e4ac265
Andreas Gyr <andygyr@gmx.ch>
2015-06-09 12:17:18 +0200

    added comments for error responses in import action

--- src/AppBundle/Controller/ImportController.php
+++ src/AppBundle/Controller/ImportController.php
@@ -18,33 +18,31 @@ class ImportController extends Controller
 
         // Check request http method
         if($request->getMethod() != 'POST') {
-            //return new Response('Invalid method!', Response::HTTP_METHOD_NOT_ALLOWED);
-            return new Response($request->getContent(), Response::HTTP_METHOD_NOT_ALLOWED);
+            return new Response('Invalid method!', Response::HTTP_METHOD_NOT_ALLOWED);
         }
 
         // Check request http header "Content-Type"
         if($request->getContentType() != 'xml') {
-            //return new Response('Invalid content-type: '.$request->getContentType(), Response::HTTP_UNSUPPORTED_MEDIA_TYPE);
-            return new Response($request->getContent(), Response::HTTP_UNSUPPORTED_MEDIA_TYPE);
+            return new Response('Invalid content-type: '.$request->getContentType(), Response::HTTP_UNSUPPORTED_MEDIA_TYPE);
         }
 
+        // Check if request body is empty
         if($request->getContent() == '') {
-            return new Response($request->getContent(), Response::HTTP_NOT_FOUND);
+            return new Response('Empty request body!', Response::HTTP_NOT_FOUND);
         }
 
         // Check if XML is valid
         $xml = $request->getContent();
         $xsdFilename = $this->get('kernel')->locateResource("@AppBundle/Resources/schema/composition.xsd");
         if(!$this->get("app.xsd_validator")->isValid($xml, $xsdFilename)) {
-            //return new Response('XML is not valid!', Response::HTTP_BAD_REQUEST);
-            return new Response($request->getContent(), Response::HTTP_BAD_REQUEST);
+            return new Response('XML is not valid!', Response::HTTP_BAD_REQUEST);
         }
 
         // Store Composition
         if(!$this->get("app.composition_controller")->storeFromXML($xml)) {
-            //return new Response('XML could not be converted into an object!', Response::HTTP_BAD_REQUEST);
-            return new Response($request->getContent(), Response::HTTP_BAD_REQUEST);
+            return new Response('XML could not be converted into an object!', Response::HTTP_NOT_IMPLEMENTED);
         }
+
         return new Response("Success!");
     }
 }
