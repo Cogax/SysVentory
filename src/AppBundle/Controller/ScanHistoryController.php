a5942534c882ad3451fc93ec3f569dd2833a3da5
Andreas Gyr <andygyr@gmx.ch>
2015-06-13 16:56:07 +0200

    removed scanhistory show action

--- src/AppBundle/Controller/ScanHistoryController.php
+++ src/AppBundle/Controller/ScanHistoryController.php
@@ -29,24 +29,4 @@ class ScanHistoryController extends Controller
             'entities' => $entities,
         ));
     }
-
-    /**
-     * Finds and displays a ScanHistory entity.
-     *
-     */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('AppBundle:ScanHistory')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find ScanHistory entity.');
-        }
-
-
-        return $this->render('AppBundle:ScanHistory:show.html.twig', array(
-            'entity'      => $entity,
-        ));
-    }
 }
