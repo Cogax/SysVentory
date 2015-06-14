a99e1e46032b62c0fcfe730bfa2126f526b627ba
Andreas Gyr <andygyr@gmx.ch>
2015-06-14 16:42:34 +0200

    changed tests with login

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,13 +4,13 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+abstract class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/login');
         $this->assertGreaterThan(0, $crawler->filter('html:contains("SysVentory")')->count(), 'Missing Logo element!');
     }
 
--- src/AppBundle/Tests/Controller/ScanControllerTest.php
+++ src/AppBundle/Tests/Controller/ScanControllerTest.php
@@ -2,53 +2,55 @@
 
 namespace AppBundle\Tests\Controller;
 
-use AppBundle\DataFixtures\Tests\LoadUserData;
+use AppBundle\DataFixtures\LoadUserData;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class ScanControllerTest extends WebTestCase
 {
+    private $client;
+
     public function setup() {
-        $client = static::createClient();
-        $container = $client->getContainer();
+        $this->client = static::createClient();
+        $container = $this->client->getContainer();
         $doctrine = $container->get('doctrine');
         $entityManager = $doctrine->getManager();
 
         $fixture = new LoadUserData();
         $fixture->load($entityManager);
+
+        $this->doLogin('inventor', 'inventor');
     }
 
     public function testCreateTwoNetworkWithSameNetRangeAndName()
     {
-        //$this->login('testadmin', 'testadmin');
+        $this->createNetwork('123', 'test');
 
-        $client = $this->createNetwork('123', 'test');
-        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
 
-        $client = $this->createNetwork('123', 'test');
-        $crawler = $client->getCrawler();
+        $this->createNetwork('123', 'test');
+        $crawler = $this->client->getCrawler();
         $this->assertEquals(2, $crawler->filter('li:contains("already in use")')->count());
     }
 
-    private function login($username, $password) {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
+    private function doLogin($username, $password) {
+        $crawler = $this->client->request('GET', '/login');
         $form = $crawler->selectButton('_submit')->form(array(
           '_username'  => $username,
           '_password'  => $password,
         ));
-        $client->submit($form);
+        $this->client->submit($form);
 
-        $this->assertTrue($client->getResponse()->isRedirect());
+        $this->assertTrue($this->client->getResponse()->isRedirect());
 
-        $crawler = $client->followRedirect();
+        $crawler = $this->client->followRedirect();
     }
 
     private function createNetwork($range, $name) {
-        $client = static::createClient();
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request('GET', '/scan/new');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
 
-        $crawler = $client->request('GET', '/scan/new');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
         $form = $crawler->selectButton('Scan')->form(array(
           'appbundle_scan[range]'  => $range,
@@ -56,7 +58,6 @@ class ScanControllerTest extends WebTestCase
           'appbundle_scan[name]'  => $name,
         ));
 
-        $client->submit($form);
-        return $client;
+        $this->client->submit($form);
     }
 }
